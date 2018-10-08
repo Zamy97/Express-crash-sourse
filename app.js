c18e7423596bdb6ca5f670344414a32dc114fb60
Akhtar Zaman <auz.zamy@gmail.com>
2018-10-07 22:39:45 -0700

    middle ware

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ var path = require('path');
 
 var app = express();
 
+// var logger = function(req, res, next){
+//     console.log("logging...");
+//     next();
+//
+// }
+//
+// app.use(logger);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set static path
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 app.get('/',function(req,res){
     res.send("Hello world");
 })
-
-
-
-
 app.listen(5000, function(){
     console.log("server started on port 5000");
 })
