52ba868e2da786b1670b5c08b7c7226d858521c1
Akhtar Zaman <auz.zamy@gmail.com>
2018-10-07 22:19:12 -0700

    adding middleware

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+
+
+var app = express();
+
+app.get('/',function(req,res){
+    res.send("Hello world");
+})
+
+
+
+
+app.listen(5000, function(){
+    console.log("server started on port 5000");
+})
